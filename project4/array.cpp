042aea01768836db9c001afd78f461949476ca65
Paul Zhang <paulz@ucla.edu>
2022-02-03 16:50:58 -0800

    Add declarations for required functions
    As specified in the instructions.

---
+++ project4/array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int locateMinimum(const string array[], int n);
+
+int countCommonElements(const string array1[], int n1, const string array2[],
+                        int n2);
+
+int countVowels(const string array[], int n);
+
+bool hasThreeOfX(const string array[], int n, string x);
+
+string majorityElement(const string array[], int n);
+
+bool shiftRight(string array[], int n, int amount, string placeholder);
+
+bool rotateLeft(string array[], int n, int amount);
+
+int replaceAll(string array[], int n, char letterToReplace, char letterToFill);
+
+int main() {
+    return 0;
+}
