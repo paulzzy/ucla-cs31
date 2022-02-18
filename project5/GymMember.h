b24a65d2460ac85f5e19d3c2496fb428310638e7
Paul Zhang <paulz@ucla.edu>
2022-02-18 12:38:04 -0800

    Implement public methods for `GymMember`

--- project5/GymMember.h
+++ project5/GymMember.h
@@ -10,6 +10,45 @@ class GymMember {
     std::string mAccountNumber;
     Kind mKind;
     int mWorkoutCount = 0;
+
+   public:
+    GymMember(std::string name, std::string accountnumber, Kind kind) {
+        mName = name;
+        mAccountNumber = accountnumber;
+        mKind = kind;
+    }
+
+    int workoutsThisMonth() { return mWorkoutCount; }
+
+    void startNewMonth() { mWorkoutCount = 0; }
+
+    void newWorkout() { mWorkoutCount++; }
+
+    Kind getKind() { return mKind; }
+
+    std::string getKindAsString() {
+        switch (mKind) {
+            case OTHER:
+                return "OTHER";
+                break;
+            case REGULAR:
+                return "REGULAR";
+                break;
+            case PREMIER:
+                return "PREMIER";
+                break;
+            case EXECUTIVE:
+                return "EXECUTIVE";
+                break;
+            default:
+                return "SOMETHING_HAS_GONE_TERRIBLY_WRONG";
+                break;
+        }
+    }
+
+    std::string getName() { return mName; }
+
+    std::string getAccountNumber() { return mAccountNumber; }
 };
 
 #endif
