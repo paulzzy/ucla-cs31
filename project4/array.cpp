62d33675acfbecfa890d696f574acb4eb3525f6e
Paul Zhang <paulz@ucla.edu>
2022-02-17 17:09:59 -0800

    Add comprehensive tests for Project 4

--- project4/array.cpp
+++ project4/array.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cassert>
 
 using namespace std;
 
@@ -191,4 +192,103 @@ int replaceAll(string array[], int n, char letterToReplace, char letterToFill) {
     }
 }
 
-int main() { return 0; }
+// Helper function for testing
+bool areArraysEqual(string array1[], string array2[], int n) {
+    for (int i = 0; i < n; i++) {
+        if (array1[i] != array2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int main() {
+    string test_0[1];
+    string test_1[8] = {"samwell", "jon",   "margaery",  "daenerys",
+                        "tyrion",  "sansa", "magdalena", "jon"};
+    string test_2[5] = {"mama", "mama", "12", "sansa", "mama"};
+    string test_3[7] = {"a", "b", "b", "A", "C", "c", "R"};
+    string test_4[10] = {"C", "C++", "Java", "Python",     "Bash",
+                         "R", "Go",  "Rust", "JavaScript", "PHP"};
+    string test_5[4] = {"identical", "identical", "identical", "identical"};
+    string test_6[1] = {""};
+    string test_7[5] = {"XYZ", "Abc", "XYZ", "zzZ", "XYZ"};
+
+    // locateMinimum()
+    assert(locateMinimum(test_1, 8) == 3);
+    assert(locateMinimum(test_2, 5) == 2);
+    assert(locateMinimum(test_3, 7) == 3);
+    assert(locateMinimum(test_4, 10) == 4);
+    assert(locateMinimum(test_0, 0) == -1);
+    assert(locateMinimum(test_6, 1) == 0);
+    assert(locateMinimum(test_7, 5) == 1);
+
+    // countCommonElements()
+    assert(countCommonElements(test_1, 8, test_2, 5) == 1);
+    assert(countCommonElements(test_1, 8, test_3, 6) == 0);
+    assert(countCommonElements(test_3, 7, test_4, 10) == 2);
+    assert(countCommonElements(test_0, 0, test_0, 0) == -1);
+    assert(countCommonElements(test_6, 1, test_6, 1) == 1);
+    assert(countCommonElements(test_5, 4, test_5, 4) == 16);
+    assert(countCommonElements(test_7, 5, test_7, 5) == 11);
+
+    // countVowels()
+    assert(countVowels(test_2, 5) == 8);
+    assert(countVowels(test_1, 8) == 21);
+    assert(countVowels(test_0, 0) == -1);
+    assert(countVowels(test_6, 1) == 0);
+
+    // hasThreeOfX()
+    assert(!hasThreeOfX(test_1, 8, "jon"));
+    assert(hasThreeOfX(test_2, 5, "mama"));
+    assert(!hasThreeOfX(test_5, 4, "identical"));
+    assert(!hasThreeOfX(test_0, 0, ""));
+    assert(!hasThreeOfX(test_6, 1, ""));
+    assert(hasThreeOfX(test_7, 5, "XYZ"));
+    assert(!hasThreeOfX(test_7, 5, "Abc"));
+
+    // majorityElement()
+    assert(majorityElement(test_2, 5) == "mama");
+    assert(majorityElement(test_5, 4) == "identical");
+    assert(majorityElement(test_6, 1) == "");
+    assert(majorityElement(test_0, 0) == "");
+    assert(majorityElement(test_7, 5) == "XYZ");
+
+    // shiftRight()
+    assert(shiftRight(test_1, 8, 2, "lol, lmao"));
+    string test_1_modified[8] = {"lol, lmao", "lol, lmao", "samwell", "jon",
+                                 "margaery",  "daenerys",  "tyrion",  "sansa"};
+    assert(areArraysEqual(test_1, test_1_modified, 8));
+    assert(shiftRight(test_5, 4, 5, "lol, lmao"));
+    string test_5_modified[4] = {"lol, lmao", "lol, lmao", "lol, lmao",
+                                 "lol, lmao"};
+    assert(areArraysEqual(test_5, test_5_modified, 4));
+    assert(!shiftRight(test_0, 0, 1, "lol, lmao"));
+
+    // rotateLeft()
+    assert(!rotateLeft(test_1, 8, 8));
+    assert(areArraysEqual(test_1, test_1_modified, 5));
+    assert(rotateLeft(test_4, 10, 4));
+    string test_4_modified[10] = {"Bash", "R", "Go",  "Rust", "JavaScript",
+                                  "PHP",  "C", "C++", "Java", "Python"};
+    assert(areArraysEqual(test_4, test_4_modified, 10));
+    assert(!rotateLeft(test_0, 0, 1));
+    string test_1_copy[8] = {"samwell", "jon",   "margaery",  "daenerys",
+                             "tyrion",  "sansa", "magdalena", "jon"};
+    string test_1_copy_modified[8] = {"margaery", "daenerys",  "tyrion",
+                                      "sansa",    "magdalena", "jon",
+                                      "samwell",  "jon"};
+    assert(rotateLeft(test_1_copy, 8, 2));
+    assert(areArraysEqual(test_1_copy, test_1_copy_modified, 8));
+
+    // replaceAll()
+    assert(replaceAll(test_7, 5, 'X', 'W') == 3);
+    string test_7_modified[5] = {"WYZ", "Abc", "WYZ", "zzZ", "WYZ"};
+    assert(areArraysEqual(test_7, test_7_modified, 5));
+    assert(replaceAll(test_6, 1, 'X', 'W') == 0);
+    assert(replaceAll(test_0, 0, '\0', 'W') == -1);
+
+    cerr << "Reached end of tests" << endl;
+
+    return 0;
+}
