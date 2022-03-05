0b2772fec7e715f369b7d6a1cd89cd55d5f96879
Paul Zhang <paulz@ucla.edu>
2022-03-05 12:21:15 -0800

    Complete TODOs for the `Board` class

--- project7/Board.cpp
+++ project7/Board.cpp
@@ -6,69 +6,68 @@
 //  Copyright © 2017 Howard Stahl. All rights reserved.
 //
 
+// DOOT means a finished TODO
+// It's also very funny
+// "DOOT"
+
 #include "Board.h"
 
-
-namespace cs31
-{
-    Board::Board() : mRound( 0 )
-    {
-        // initialize each BoardRow
-        for (int i = 1; i <= 6; i++)
-        {
-            mBoardRow[ i ].setRound( i );
-        }
+namespace cs31 {
+Board::Board() : mRound(0) {
+    // initialize each BoardRow
+    for (int i = 1; i <= 6; i++) {
+        mBoardRow[i].setRound(i);
     }
-    
-    void Board::setCurrentRound( int round )
-    {
-        // unset the current board row
-        if (mRound >= 0 && mRound <= 6)
-            mBoardRow[ mRound ].setCurrentRound( false );
-        mRound = round;
-        // set the current board row
-        if (mRound >=0 && mRound <=6)
-            mBoardRow[ mRound ].setCurrentRound(true);
-    }
-    
-    // TODO: set the human player to have won this current BoardRow
-    void Board::markHumanAsWinner()
-    {
-        // stubbed out for now...
-    }
-    
-    // TODO: set the computer player to have won this current BoardRow
-    void Board::markComputerAsWinner()
-    {
-        // stubbed out for now...
-    }
-    
-    // stringify the Board
-    std::string Board::display( ) const
-    {
-        std::string s = "\t\t  Bunco Game\n\tHuman\t\t\tComputer\n";
-        
-        for( int i = 1; i <= 6; i++)
-        {
-            s += mBoardRow[ i ].display() + "\n";
-        }
-        
-        return( s );
-    }
-    
-    // TODO: how many rounds has the human player won?
-    int Board::countUpHumanRoundWins( ) const
-    {
-        // stubbed out for now...
-        return( 0 );
-    }
-    
-    // TODO: how many rounds has the computer player won?
-    int Board::countUpComputerRoundWins( ) const
-    {
-        // stubbed out for now...
-        return( 0 );
-    }
-
-    
 }
+
+void Board::setCurrentRound(int round) {
+    // unset the current board row
+    if (mRound >= 0 && mRound <= 6) mBoardRow[mRound].setCurrentRound(false);
+    mRound = round;
+    // set the current board row
+    if (mRound >= 0 && mRound <= 6) mBoardRow[mRound].setCurrentRound(true);
+}
+
+// DOOT: set the human player to have won this current BoardRow
+void Board::markHumanAsWinner() {
+    BoardRow current_row = mBoardRow[mRound];
+    current_row.setHumanAsWinner();
+}
+
+// DOOT: set the computer player to have won this current BoardRow
+void Board::markComputerAsWinner() {
+    BoardRow current_row = mBoardRow[mRound];
+    current_row.setComputerAsWinner();
+}
+
+// stringify the Board
+std::string Board::display() const {
+    std::string s = "\t\t  Bunco Game\n\tHuman\t\t\tComputer\n";
+
+    for (int i = 1; i <= 6; i++) {
+        s += mBoardRow[i].display() + "\n";
+    }
+
+    return (s);
+}
+
+// DOOT: how many rounds has the human player won?
+int Board::countUpHumanRoundWins() const {
+    int human_wins = 0;
+    for (int i = 1; i <= 6; i++) {
+        human_wins = mBoardRow[i].didHumanWin() ? human_wins + 1 : human_wins;
+    }
+    return human_wins;
+}
+
+// DOOT: how many rounds has the computer player won?
+int Board::countUpComputerRoundWins() const {
+    int computer_wins = 0;
+    for (int i = 1; i <= 6; i++) {
+        computer_wins =
+            mBoardRow[i].didComputerWin() ? computer_wins + 1 : computer_wins;
+    }
+    return computer_wins;
+}
+
+}  // namespace cs31
